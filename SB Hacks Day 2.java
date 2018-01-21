46d99dfc12e6ab6c413e23269584f3ef014aac22
maitrishah101 <maitri.shah101@gmail.com>
2018-01-20 17:18:19 -0800

    SB hacks 2

---
+++ SB Hacks Day 2.java
@@ -0,0 +1,1 @@
+"Day 2 of SB Hacks. Went to get buddha bows, went to the beach, and hung out with some old friends from high school! My partner and I have a cool idea for a hack and I'm really excited!"
