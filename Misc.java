c03ed96bdbb7a079d45d7a46491beb0d09d73b78
maitrishah101 <maitri.shah101@gmail.com>
2018-02-03 22:51:12 -0800

    day 7?

--- Misc.java
+++ Misc.java
@@ -1,1 +1,1 @@
-"Hard day but today is the day I'm going to get back on track and get through all the work that I need to do, 2 projects a week is hard"
+"Hard day but today is the day I am going to get back on track and get through all the work that I need to do, 2 projects a week is hard"
