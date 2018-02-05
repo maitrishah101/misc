c5dbe206d196a3033a883b067cfa99450822f5ea
maitrishah101 <maitri.shah101@gmail.com>
2018-02-04 20:51:31 -0800

    Day 12?

--- Day Before Classes.java
+++ Day Before Classes.java
@@ -1,1 +1,1 @@
-"I had 4 interviews and 2 applications due today I'm extremely sorry but I'll make up for it as soon as possible"
+"I had 4 interviews and 2 applications due today I'm extremely sorry but I will make up for it as soon as possible"
