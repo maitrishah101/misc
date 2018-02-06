6662e9ce340548271331bb505c563bfc3070dc4b
maitrishah101 <maitri.shah101@gmail.com>
2018-02-05 22:48:37 -0800

    I need to get better at this

--- New Years.java
+++ New Years.java
@@ -1,1 +1,1 @@
-System.out.println("eek it's new years and I didn't manage my time well but day 3 lets go!! happy new year welcome 2018");
+System.out.println("eek it is new years and I didn't manage my time well but day 3 lets go!! happy new year welcome 2018");
